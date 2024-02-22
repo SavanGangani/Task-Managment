66fea71297fb368383e2e56fc1a84401e5a9ecdf
AnupRTailor <tailoranup071@gmail.com>
2024-02-22 16:48:23 +0530

    Interface added

---
+++ mvc/Repositories/ITaskRepository.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc.Repositories
+{
+    public class ITaskRepository
+    {
+        public List<MyTask> GetAllTask();
+        public MyTask GetOneTask(int id);
+        public void AddTask(MyTask task);
+        public void AddToMyTask(MyTask myTask);
+        public void UpdateStatus(MyTask myTask);
+
+        public List<MyTask> GetMyTask();
+        public List<MyTask> GetUsersTask();
+        private int GetTaskTypeId(string TaskType, NpgsqlConnection conn);
+        public void EditTask(MyTask task);
+        public void DeleteTask(MyTask task);
+    }
+}
