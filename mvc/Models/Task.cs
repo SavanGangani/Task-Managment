fcc941addb2c71b02b48615f265a847cd631f25c
apitestmail <apitestmail20@gmail.com>
2024-02-22 16:01:56 +0530

    task model added

--- mvc/Models/Task.cs
+++ mvc/Models/Task.cs
@@ -7,6 +7,19 @@ namespace mvc.Models
 {
     public class Task
     {
-        
+           public int  c_taskid { set; get; }
+
+        public string c_tasktypeid { set; get; }
+        public int c_tasktid { set; get; }
+        public string c_tasktype {set; get;}
+
+        public string c_taskissue { set; get; }
+
+        public DateTime c_initialdate { set; get; }
+
+        public DateTime c_duedate { set; get; }
+        public string c_status { set; get; }
+
+        public string c_taskusername {set; get;}
     }
 }
